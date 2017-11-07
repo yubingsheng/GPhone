961fc4b11dce357cc529b177f13a7a283680e148
DEV_IOS <332004729@qq.com>
2017-11-07 14:15:05 +0800

    Create Project

---
+++ GPhone/GPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPhone
+//
+//  Created by 郁兵生 on 2017/11/7.
+//  Copyright © 2017年 郁兵生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPhone/GPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPhone
+//
+//  Created by 郁兵生 on 2017/11/7.
+//  Copyright © 2017年 郁兵生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
